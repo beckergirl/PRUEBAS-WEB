0ac9603eb38c2f2269d111b709c23d7267f7750c
Irene Becker <116119849+beckergirl@users.noreply.github.com>
2023-06-27 15:14:36 +0200

    Update formulario.php
    corrección

--- formulario.php
+++ formulario.php
@@ -1,24 +1,24 @@
 <?php
-$nombre = $_POST["nombre"];
-$email = $_POST["email"];
-$empresa = $_POST["mensaje"];
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$empresa = $_POST['mensaje'];
 
-$header = "From: " . $mail . " \r\n ";
+$header = 'From: ' . $mail . " \r\n ";
 $header = "X-Mailer: PHP/" . phpversion() . " \r\n ";
 $header = "Mine-Version: 1.0 \r\n ";
 $header = "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . " ,\r\n ";
-$mensaje = "Su e-mail es: " . $email . " \r\n ";
+$mensaje = "Su e-mail es: " . $mail . " \r\n ";
 $mensaje = "Mensaje: " . $POST["mensaje"] . " \r\n ";
 $mensaje = "Enviado el " .date("d/m/Y" , time());
 
-$para = "ibecker.art@gmail.com";
-$asunto = "Asunto: web Irene Becker Studio";
+$para = 'ibecker.art@gmail.com';
+$asunto = 'Asunto: web Irene Becker Studio';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Locatio:index.html");
+header("Location:index.html");
 ?>
 
 
